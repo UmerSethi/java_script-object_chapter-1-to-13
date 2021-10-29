b1d9ae962cd7a094cd8b26c6baf0fffe183eb4bf
Muhammad Umer Saleem <m.umersethi91@gmail.com>
2021-10-30 03:04:25 +0500

    java_script object chapter 1 to 13

---
+++ app.js
@@ -0,0 +1,169 @@
+                            
+                            //  CHAPTER#01
+
+//alert("Hello World") 
+// alert("Error! Please enter a valid password.");
+// alert("Welcome to JS Land...\nHappy Coding!");
+// alert("Welcome to JS Land...");
+// alert("Happy Coding!\nPrevent this page from creating additional dialogs.");
+// alert("Hello... I can run js through my web browser's console");
+// 6. DONE
+// 7. DONE
+
+
+                        // CHAPTER#02
+
+// var username=umer;
+// var myName= Muhammad Umer Saleem;
+// var message = "Hello World";
+// alert(message);
+// var username = "Muhammad Umer Saleem";
+// var userAge = "20 years old";
+// var course = "Certified Web  Mobile Application Development";
+// alert(username);
+// alert(userAge);
+// alert(course);
+// alert("pizza\npizz\npiz\npi\np");
+// var email = "m.umersethi91@gmail.com";
+// alert("My email address is " + email);
+// var book = "A smarter way to learn JavaScript";
+// alert("I am trying to learn from the Book " + book);
+// document.write("Yah! I can write HTML content through JavaScript");
+// alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+
+                    //  CHAPTER#3
+
+//  var age = 15
+//  alert("I am " + age + " years old");
+            
+// var visits = 14;
+// alert("You have visited this site " + visits + " times")
+
+// var birthYear = 2001;
+// document.write("My birth year is " + birthYear + "<br>Data type of my declared variable is number");
+
+// var visitorName = "Muhammad Umer Saleem";
+// var productTitle = "Shirts";
+// var productquantity = 3;
+// document.write("<b>"+visitorName+"</b> ordered <b>"+ productquantity + " " + productTitle +"</b>(s) on XYZ Clothing store")
+
+
+                    //    CHAPTER#04
+
+// var firstName = "Muhammad Umer", lastName = "Saleem", course = "Web & App Development";
+
+// Legal Variables      // var myName;
+                        // var $name;
+                        // var _name;
+                        // var name1;
+                        // var MYNAME;
+
+// Illegal Variables    // var 1name;
+                        // var &Name;
+                        // var my name;
+                        // var #name;
+                        // var user age;
+
+
+// document.write("<h1>Rules for naming JS variables</h1> <br> <br> <br>");
+// document.write("<p>Variables names can only contain letters, numbers, $ and _. For example $my_1stVariable</p>");
+// document.write("<p>Variable must begin with a $, _ or letter. For example $name, _name or name</p>");
+// document.write("<p>Variable names are case sensitive</p>");
+// document.write("<p>Variable names should not be JS keywords</p>");
+
+
+
+            //    CHAPTER#05
+
+            // var num1 =3 ;
+            // var num2 =5 ;
+            // var sum = num1 + num2 ;
+            // document.write("Sum of " + num1 + " and " + num2 + " is " + sum );
+
+// var num1 = 3;
+// var num2 = 5;
+// var minus = num1 - num2;
+// var product = num1 * num2;
+// var quotient = num1 / num2;
+// var remainder = num1 % num2;
+// document.write("<p>Difference of " + num1 + " and " + num2 + " is " + minus + "</p>");
+// document.write("<p>Product of " + num1 + " and " + num2 + " is " + product + "</p>");
+// document.write("<p>Division of " + num1 + " and " + num2 + " is " + quotient + "</p>");
+// document.write("<p>Modulus of " + num1 + " and " + num2 + " is " + remainder + "</p>");
+
+
+
+// var table = 4;
+// document.write("<p>Table of " + table + "</p>")
+// document.write("<p>4x1=" + table*1 + "</p>")
+// document.write("<p>4x2=" + table*2 + "</p>")
+// document.write("<p>4x3=" + table*3 + "</p>")
+// document.write("<p>4x4=" + table*4 + "</p>")
+// document.write("<p>4x5=" + table*5 + "</p>")
+// document.write("<p>4x6=" + table*6 + "</p>")
+// document.write("<p>4x7=" + table*7 + "</p>")
+// document.write("<p>4x8=" + table*8 + "</p>")
+// document.write("<p>4x9=" + table*9 + "</p>")
+// document.write("<p>4x10=" + table*10 + "</p>")
+
+
+// var c = 25;
+// var fahrenheit = (c * 9 / 5) + 32;
+// var f = 70;
+// var celsius = (f - 32) * 5 / 9;
+// document.write(c + "&#176;C is " + fahrenheit + "&#176;F <br>");
+// document.write(f + "&#176;F is " + celsius + "&#176;C");
+
+
+
+// var item1_Price = 500;
+// var item2_Price = 200;
+// var item1_qty = 3;
+// var item2_qty = 7;
+// var shipCharges = 100;
+// var totalCost = (item1_Price * item1_qty) + (item2_Price * item2_qty) + shipCharges;
+// document.write("<h1> Shopping Cart </h1> <br> <br>");
+// document.write("Price of item 1 is " + item1_Price + "<br>");
+// document.write("Quantity of item 1 is " + item1_qty + "<br>");
+// document.write("Price of item 2 is " + item2_Price + "<br>");
+// document.write("Quantity of item 2 is " + item2_qty + "<br> <br>");
+// document.write("Total cost of your order is " + totalCost);
+
+
+
+// var totalMarks = 730;
+// var obtMarks = 270;
+// var per = obtMarks / totalMarks * 100;
+// document.write("<h1>Marks Sheet</h1> <br> <br>");
+// document.write("Total Marks: " + totalMarks + "<br>");
+// document.write("Marks Obtained: " + obtMarks + "<br>");
+// document.write("Percentage: " + per + "<br>");
+
+
+
+// ar dollars = 10;
+// var riyals = 25;
+// var rupees = (dollars * 104.80) + (riyals * 28);
+// document.write("<h1>Currency in PKR</h1> <br> <br>");
+// document.write("Total Currency in PKR: " + rupees);
+ 
+
+
+// ar num1 = 10;
+// var operation = (num1 + 5 * 10) / 2 
+
+
+// var r = 20;
+// var circumference = 2 * 3.142 * r;
+// var area = 3.142 * r * r;
+// document.write("<h1>The Geometrizer</h1> <br><br>");
+// document.write("Radius of a circle: " + r + "<br>")
+// document.write("The circumference is: " + circumference + "<br>")
+// document.write("The area is: " + area + "<br>")
+
+
+
+// var favSnack = "French Fries";
+// var age = 20;
+
+// document.write("<h1>The Lifetime Supply Calculator</h1> <br><br>");
